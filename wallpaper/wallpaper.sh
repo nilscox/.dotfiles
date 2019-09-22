259f09d1b79fd2c830cc952175970df068eb4707
Nils <nils@localhost>
2019-09-22 15:07:43 +0200

    fix wallpapers script

--- wallpaper/wallpaper.sh
+++ wallpaper/wallpaper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-[ -z "$WALLPAPERS_DIR" ] && WALLPAPERS_DIR="~/.wallpapers"
+[ -z "$WALLPAPERS_DIR" ] && WALLPAPERS_DIR="$HOME/.wallpapers"
 [ ! -d "$WALLPAPERS_DIR" ] && exit 0
 
 wp="$WALLPAPERS_DIR/$(ls "$WALLPAPERS_DIR" | sort -R | tail -1)"
