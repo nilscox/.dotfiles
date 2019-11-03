701589a77529ba55cc9716f7de8c8ebe76e85a72
Nils <nils@epita.fr>
2019-11-03 14:43:45 +0100

    update git setup

--- setup/git.sh
+++ setup/git.sh
@@ -3,7 +3,7 @@
 source "$DOT/setup/functions.sh"
 
 run_install() {
-  ln -sf "$DOT/git/gitconfig" "$DEST/.gitconfig"
+  cp "$DOT/git/gitconfig" "$DEST/.gitconfig"
   ln -sf "$DOT/git/gitignore_global" "$DEST/.gitignore_global"
 }
 
