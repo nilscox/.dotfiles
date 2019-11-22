933bd93a59b1dfbc70abf71e78468b53469d0637
Nils <nils@epita.fr>
2019-11-22 16:33:50 +0100

    fix move scratchpad script on !== screen resolutions

--- i3/move-scratchpad.sh
+++ i3/move-scratchpad.sh
@@ -4,16 +4,20 @@ wid=$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')
 wname=$(xprop -id "$wid" | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)
 test "$wname" != "scratchpad" && exit 0
 
-y="22"
+offset_y="22"
 width="660"
 
 rect=$(i3-msg -t get_tree | jq -r '.nodes[] | select([recurse(.nodes[]?, .floating_nodes[]?) | .focused] | any) | .rect')
 scrn_x=$(echo "$rect" | jq -r '.x')
+scrn_y=$(echo "$rect" | jq -r '.y')
 scrn_width=$(echo "$rect" | jq -r '.width')
 scrn_height=$(echo "$rect" | jq -r '.height')
 
 x=$(($scrn_x + $scrn_width - $width))
-height=$(($scrn_height - $y))
+y=$(($scrn_y + $offset_y))
+height=$(($scrn_height - $offset_y))
+
+echo "$x" "$y"
 
 i3-msg resize set "$width" px "$height" px \
   && i3-msg move absolute position "$x" px "$y" px
