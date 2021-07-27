a2573f9a5b0706b75c4ac4dd8947a986bde4000c
Nils <nils@epita.fr>
2021-07-27 12:49:28 +0200

    add global npm scripts

--- node/node.sh
+++ node/node.sh
@@ -7,3 +7,10 @@ clone_or_pull 'git://github.com/creationix/nvm.git' "$dest/.nvm"
 source "$dest/.nvm/nvm.sh"
 nvm install --lts
 nvm use --lts
+
+npm i -g yarn
+npm i -g typescript
+npm i -g ts-node
+npm i -g http-server
+npm i -g nodemon
+npm i -g uuid
