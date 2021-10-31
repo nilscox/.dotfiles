d9614349fce92b1b03fe9e9e58ee057889338335
Nils <nils@epita.fr>
2021-10-31 13:06:54 +0100

    update zsh config

--- zsh/zshrc.dev.sh
+++ zsh/zshrc.dev.sh
@@ -1,15 +1,24 @@
-export LOCAL_IP="$(ip a | grep 'inet .* wlp2s0' | sed -E 's,.*inet ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+).*,\1,')"
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 export DEBUG_PRINT_LIMIT=100000
 export TS_NODE_TRANSPILE_ONLY=true
-export NODE_OPTIONS='--max-old-space-size=8192'
+#export NODE_OPTIONS='--max-old-space-size=8192'
+
+LOCAL_IP="$(ip a | grep 'inet .* wlp2s0' | sed -E 's,.*inet ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+).*,\1,')"
+
+NVM_COMPLETION="true"
+NVM_LAZY_LOAD="false"
+NVM_AUTO_USE="true"
 
 alias y='yarn'
+alias yt='yarn test'
 alias ytw='yarn test --watch --notify=false'
+alias yb='yarn build'
+alias yl='yarn lint'
+alias ys='yarn start'
+alias yui='yarn upgrade-interactive'
 alias ya='yarn add'
 alias yad='yarn add -D'
 alias yar='yarn remove'
-alias yui='yarn upgrade-interactive'
-alias ys='yarn start'
 
 alias g='git'
 alias p='python'
