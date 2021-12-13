dc5e74ed189f276225ab0de03a6436ccafda5d0c
nils <nils@epita.fr>
2021-12-13 21:23:01 +0100

    fix polybar config

--- polybar/polybar.sh
+++ polybar/polybar.sh
@@ -10,7 +10,8 @@ ln -s "$dot/polybar/modules" "$config/polybar/modules"
 mkdir -p "$config/polybar/scripts"
 for script in "$dot"/polybar/scripts/*; do ln -s "$script" "$config/polybar/scripts"; done
 
-ln -s "$dot/polybar/polybar-config" "$dest/.local/bin"
+ln -s "$dot/polybar/polybar-config.sh" "$dest/.local/bin"
+ln -s "$dot/polybar/launch.sh" "$dest/.local/bin/polybar.sh"
 
 ln -s "$dot/polybar/polybar.service" "$config/systemd/user"
 systemctl --user daemon-reload
