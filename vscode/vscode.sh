9823d488746d52e6bb9d159cb466ce96bb5a11d2
nils <nils@epita.fr>
2022-02-22 15:22:46 +0100

    update vscode config

--- vscode/vscode.sh
+++ vscode/vscode.sh
@@ -25,8 +25,20 @@ extensions=(
   ibm.output-colorizer
   ryanluker.vscode-coverage-gutters
   ms-vscode.vscode-typescript-tslint-plugin
+  bradlc.vscode-tailwindcss
   streetsidesoftware.code-spell-checker
+  streetsidesoftware.code-spell-checker-french
+  graphql.vscode-graphql
 )
+
+# https://code.visualstudio.com/docs/setup/linux#_rhel-fedora-and-centos-based-distributions
+if [ $distrib == 'fedora' ]; then
+  sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+  sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+  dnf check-update
+  sudo dnf install code
+fi
 
 mkdir -p "$vscode_dir/User"
 
