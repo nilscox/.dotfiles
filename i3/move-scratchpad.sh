cfd614db0c6b8b7951109e57822c7c5c64bfc246
nils <nils@koyeb.com>
2022-05-06 16:26:59 +0200

    setup a scratchpad on sway

--- i3/move-scratchpad.sh
+++ i3/move-scratchpad.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-sway 'resize set width 720 height 1062'
-sway 'move absolute position 1202 18'
-exit 0
-
 wid=$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')
 wname=$(xprop -id "$wid" | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)
 test "$wname" != "scratchpad" && exit 0
@@ -14,26 +10,27 @@ scrn_y=$(echo "$rect" | jq -r '.y')
 scrn_width=$(echo "$rect" | jq -r '.width')
 scrn_height=$(echo "$rect" | jq -r '.height')
 
-bar_height="18"
+top_bar_height="21"
+bottom_bar_height="10"
 width="660"
 height=$(($scrn_height))
 
-x=$(($scrn_x + $scrn_width - $width))
+x=$(($scrn_x + $scrn_width - $width + 2))
 y=$(($scrn_y))
 
 source "$XDG_RUNTIME_DIR/polybar.env"
 
 if [ "$BAR_TOP" = '1' ]; then
-  y=$(($y + "$bar_height"))
-  height=$(($height - $bar_height))
+  y=$(($y + "$top_bar_height"))
+  height=$(($height - $top_bar_height))
 fi
 
 if [ "$BAR_BOTTOM" = '1' ]; then
-  height=$(($height - $bar_height))
+  height=$(($height - $bottom_bar_height))
 fi
 
-# i3-msg resize set "$width" px "$height" px \
-#   && i3-msg move absolute position "$x" px "$y" px
+i3-msg resize set "$width" px "$height" px \
+  && i3-msg move absolute position "$x" px "$y" px
 
 # echo "x = $x"
 # echo "x = $x"
---
+++ sway/move-scratchpad.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+outputs=$(swaymsg -r -t get_outputs)
+
+mode=$(echo "$outputs" | jq '.[0].current_mode')
+
+screen_width=$(echo "$mode" | jq '.width')
+screen_height=$(echo "$mode" | jq '.height')
+
+term_width='720'
+term_height="$(($screen_height - 33))"
+
+term_x="$(($screen_width - $term_width + 2))"
+term_y='0'
+
+swaymsg "resize set width $term_width height $term_height"
+swaymsg "move position $term_x $term_y"
