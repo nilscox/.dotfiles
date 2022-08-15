d944287da5a00a43833bee9ca71212d07d7ba6b3
nils <nils@epita.fr>
2022-08-15 13:38:25 +0200

    add sway services

--- sway/sway.sh
+++ sway/sway.sh
@@ -8,3 +8,13 @@ mkdir -p "$config/sway"
 ln -sf "$dot/sway/config.sway" "$config/sway/config"
 ln -sf "$dot/sway/config.d" "$config/sway"
 ln -sf "$dot/sway/scripts" "$config/sway"
+
+ln -sf "$dot/sway/systemd/sway-session.target" "$config/systemd/user"
+ln -sf "$dot/sway/systemd/kanshi.service" "$config/systemd/user"
+ln -sf "$dot/sway/systemd/mako.service" "$config/systemd/user"
+ln -sf "$dot/sway/systemd/wallpaper.service" "$config/systemd/user"
+ln -sf "$dot/sway/systemd/wallpaper.timer" "$config/systemd/user"
+
+systemctl --user enable kanshi
+systemctl --user enable mako
+systemctl --user enable wallpaper
