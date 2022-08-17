cce00ad1a7c1608a77b558b75b061e99828c18e9
nils <nils@epita.fr>
2022-08-17 09:44:13 +0200

    update vim install script

--- vim/vim.sh
+++ vim/vim.sh
@@ -3,7 +3,7 @@
 source "$DOT/functions.sh"
 set -xeo pipefail
 
-ln -s "$dot/vim/vimrc" "$dest/.vimrc"
-clone_or_pull 'git@github.com:VundleVim/Vundle.vim.git' "$dest/.vim/bundle/Vundle.vim"
+ln -sf "$dot/vim/vimrc" "$dest/.vimrc"
+clone_or_pull 'https://github.com/VundleVim/Vundle.vim.git' "$dest/.vim/bundle/Vundle.vim"
 
 vim +PluginInstall +qall
